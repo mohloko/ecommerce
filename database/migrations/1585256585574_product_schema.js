9fe4451d57cd961d321919cc93233d9c63b88900
Cleber Toguchi <mohloko@gmail.com>
2020-03-26 18:12:52 -0300

    migrations

---
+++ database/migrations/1585256585574_product_schema.js
@@ -0,0 +1,34 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ProductSchema extends Schema {
+  up () {
+    this.create('products', (table) => {
+      table.increments()
+      table.string('name')
+      table.integer('image_id').unsigned
+      table.text('decription')
+      table.decimal('price', 12, 2)
+      table.timestamps()
+
+      table.foreign('image_id').references('id').inTable('images').onDelete('cascade')
+    })
+
+    this.create('image_product', (table) => {
+      table.increments()
+      table.integer('image_id').unsigned()
+      table.integer('product_id').unsigned()
+      table.timestamps()
+      table.foreign('product_id').references('id').inTable('products').onDelete('cascade')
+    })
+  }
+
+  down () {
+    this.drop('products')
+    this.drop('image_products')
+  }
+}
+
+module.exports = ProductSchema
