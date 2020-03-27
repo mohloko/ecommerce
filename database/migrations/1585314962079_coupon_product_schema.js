a0fef1ccdbdb43eb7ae6666c17733e756b3c29fe
Cleber Toguchi <mohloko@gmail.com>
2020-03-27 10:16:50 -0300

    coupon product relatinship

---
+++ database/migrations/1585314962079_coupon_product_schema.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CouponProductSchema extends Schema {
+  up () {
+    this.create('coupon_products', (table) => {
+      table.increments()
+      table.integer('coupon_id').unsigned
+      table.integer('product_id').unsigned
+      table.timestamps()
+
+      table.foreign('coupon_id').references('id').onTable('users').onDelete('cascade')
+      table.foreign('product_id').references('id').onTable('products').onDelete('cascade')
+    })
+  }
+    })
+  }
+
+  down () {
+    this.drop('coupon_products')
+  }
+}
+
+module.exports = CouponProductSchema
