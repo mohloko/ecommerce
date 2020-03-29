371bd2c219887fdcba034b78231dd543132755d5
Cleber Toguchi <mohloko@gmail.com>
2020-03-29 00:53:51 -0300

    adicionando relacionamento no User model

--- app/Models/User.js
+++ app/Models/User.js
@@ -21,6 +21,10 @@ class User extends Model {
     })
   }
 
+  static get hidden() {
+    return password
+  }
+
   static get traits () {
     return [
       '@provider:Adonis/Acl/HasRole',
@@ -41,6 +45,15 @@ class User extends Model {
   tokens () {
     return this.hasMany('App/Models/Token')
   }
+
+  image(){
+    return this.belongsTo('App/Models/Image')
+  }
+
+  coupons(){
+    return this.belongsToMany('App/Models/Coupon')
+  }
+
 }
 
 module.exports = User
